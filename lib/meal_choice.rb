79827b39c06e396f7cae32bc93815cf388f37ae5
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-26 19:39:59 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,5 @@
-# Your code here
+def meal_choice(vegetable,carb,protein="meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{vegetable} and #{carb}."
+"A plate of #{protein} with #{vegetable} and #{carb}."
+end
